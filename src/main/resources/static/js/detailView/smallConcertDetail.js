155e9f9b3e803b115af6d6589a4c3ce5f3d952da
smc <minchae.shin1@gmail.com>
2023-06-26 01:29:52 +0900

    detailView(소규모공연 예매 및 결제)

--- src/main/resources/static/js/detailView/smallConcertDetail.js
+++ src/main/resources/static/js/detailView/smallConcertDetail.js
@@ -51,7 +51,7 @@ window.onload = function() {
 
         // 로그아웃
         const logoutBtn = document.createElement("a");
-        logoutBtn.setAttribute("href", window.location.href);
+        logoutBtn.setAttribute("href", "/map");
         logoutBtn.onclick = function() {
             window.sessionStorage.removeItem("jwtToken");
         };
@@ -147,7 +147,7 @@ const reservation = document.querySelector('#reservation');
 reservation.addEventListener('click', () => {
     const jwtToken = window.sessionStorage.getItem("jwtToken");
     if (jwtToken !== null) {
-        window.location.href = `../../../templates/detailView/${id}/calendar`;
+        window.location.href = `/smallconcert/detail/${id}/calendar`;
     }else {
         alert("로그인 후 이용해주세요.");
     }
