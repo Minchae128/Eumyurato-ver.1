3bedf5c3f013eeebdf6d0d9d7eb6438b9528d704
smc <minchae.shin1@gmail.com>
2023-06-26 09:42:41 +0900

    pay(소규모공연 예매 - 공연선택확인)

--- src/main/resources/static/js/pay/reservList.js
+++ src/main/resources/static/js/pay/reservList.js
@@ -51,7 +51,7 @@ window.onload = function() {
 
         // 로그아웃
         const logoutBtn = document.createElement("a");
-        logoutBtn.setAttribute("href", "/map");
+        logoutBtn.setAttribute("href", window.location.href);
         logoutBtn.onclick = function() {
             window.sessionStorage.removeItem("jwtToken");
         };
