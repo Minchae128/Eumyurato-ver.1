3ca5a90fea55e45cf2ec819dbd9182f3e5473fd2
smc <minchae.shin1@gmail.com>
2023-07-02 15:41:15 +0900

    profile(마이페이지 - 관리자 - 공연관리)

--- src/main/resources/static/js/profile/profile_concert.js
+++ src/main/resources/static/js/profile/profile_concert.js
@@ -30,8 +30,7 @@ function getEvents(category, page = 1, searchColumn = null, searchKeyword = '') 
     }
 
     fetch(url, {
-        method: 'GET',
-        headers: {
+        method: 'GET', headers: {
             'Content-Type': 'application/json',
         },
     })
@@ -50,156 +49,155 @@ function getEvents(category, page = 1, searchColumn = null, searchKeyword = '') 
         });
 }
 
-    function displayEvents(events, category, currentPage) {
-        const eventTbody = document.getElementById('eventTbody');
-        eventTbody.innerHTML = '';
+function displayEvents(events, category, currentPage) {
+    const eventTbody = document.getElementById('eventTbody');
+    eventTbody.innerHTML = '';
 
-        const eventsPerPage = 5;
-        const start = (currentPage - 1) * eventsPerPage;
-        const end = start + eventsPerPage;
+    const eventsPerPage = 5;
+    const start = (currentPage - 1) * eventsPerPage;
+    const end = start + eventsPerPage;
 
-        const currentDate = new Date();
+    const currentDate = new Date();
 
-        events.slice(start, end).forEach((event) => {
-            const eventRow = eventTbody.insertRow();
-            const eventId = event.id;
+    events.slice(start, end).forEach((event) => {
+        const eventRow = eventTbody.insertRow();
+        const eventId = event.id;
 
-            // 각 이벤트 행에 삭제 버튼 추가
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = '삭제';
-            deleteButton.classList.add('delete-btn'); //버튼 class 지정
-            deleteButton.setAttribute('data-category', category);
-            deleteButton.setAttribute('data-id', eventId);
-            deleteButton.addEventListener('click', () => {
-                console.log(category, eventId);
-                deleteConcert(category,eventId);
-            });
-
-            // 기본 공통 컬럼
-
-            switch (category) {
-                case 'busking':
-
-                    const startBuskingDate = new Date(event.date);
-
-                    // 버스킹 공연에 대한 데이터를 생성
-                    eventRow.insertCell().textContent = event.name;
-                    eventRow.insertCell().textContent = event.artId;
-                    eventRow.insertCell().textContent = event.location;
-                    eventRow.insertCell().textContent = event.date;
-                    eventRow.insertCell().textContent = event.regDate;
-
-                    // 현재 날짜 이전 예약은 취소 불가
-                    if (startBuskingDate >= currentDate) {
-                        eventRow.insertCell().appendChild(deleteButton);
-                    } else {
-                        eventRow.insertCell().textContent = '취소 불가';
-                    }
-                    break;
-
-                case 'smallconcert':
-                    // 소규모 공연에 대한 데이터를 생성
-                    const startConcertDate = new Date(event.startDate);
-
-                    eventRow.insertCell().textContent = event.name;
-                    eventRow.insertCell().textContent = event.enterId;
-                    eventRow.insertCell().textContent = event.pname;
-                    eventRow.insertCell().textContent = event.location;
-                    eventRow.insertCell().textContent = event.startDate;
-                    eventRow.insertCell().textContent = event.lastDate;
-                    eventRow.insertCell().textContent = event.price;
-
-                    // 현재 날짜 이전 예약은 취소 불가
-                    if (startConcertDate >= currentDate) {
-                        eventRow.insertCell().appendChild(deleteButton);
-                    } else {
-                        eventRow.insertCell().textContent = '취소 불가';
-                    }
-
-                    break;
-
-                case 'localfestival':
-                    // 지역 축제에 대한 데이터를 생성
-
-                    const startDate = new Date(event.startDate);
-
-                    eventRow.insertCell().textContent = event.name;
-                    eventRow.insertCell().textContent = event.org;
-                    eventRow.insertCell().textContent = event.location;
-                    eventRow.insertCell().textContent = event.startDate;
-                    eventRow.insertCell().textContent = event.lastDate;
-
-                    // 현재 날짜 이전 예약은 취소 불가
-                    if (startDate >= currentDate) {
-                        eventRow.insertCell().appendChild(deleteButton);
-                    } else {
-                        eventRow.insertCell().textContent = '취소 불가';
-                    }
-
-                    break;
-
-                default:
-                    break;
-            }
+        // 각 이벤트 행에 삭제 버튼 추가
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '삭제';
+        deleteButton.classList.add('delete-btn'); //버튼 class 지정
+        deleteButton.setAttribute('data-category', category);
+        deleteButton.setAttribute('data-id', eventId);
+        deleteButton.addEventListener('click', () => {
+            console.log(category, eventId);
+            deleteConcert(category, eventId);
         });
 
-        createPagination(events.length, eventsPerPage, currentPage, category);
+        // 기본 공통 컬럼
+
+        switch (category) {
+            case 'busking':
+
+                const startBuskingDate = new Date(event.date);
+
+                // 버스킹 공연에 대한 데이터를 생성
+                eventRow.insertCell().textContent = event.name;
+                eventRow.insertCell().textContent = event.artId;
+                eventRow.insertCell().textContent = event.location;
+                eventRow.insertCell().textContent = event.date;
+                eventRow.insertCell().textContent = event.regDate;
+
+                // 현재 날짜 이전 예약은 취소 불가
+                if (startBuskingDate >= currentDate) {
+                    eventRow.insertCell().appendChild(deleteButton);
+                } else {
+                    eventRow.insertCell().textContent = '취소 불가';
+                }
+                break;
+
+            case 'smallconcert':
+                // 소규모 공연에 대한 데이터를 생성
+                const startConcertDate = new Date(event.startDate);
+
+                eventRow.insertCell().textContent = event.name;
+                eventRow.insertCell().textContent = event.enterId;
+                eventRow.insertCell().textContent = event.pname;
+                eventRow.insertCell().textContent = event.location;
+                eventRow.insertCell().textContent = event.startDate;
+                eventRow.insertCell().textContent = event.lastDate;
+                eventRow.insertCell().textContent = event.price;
+
+                // 현재 날짜 이전 예약은 취소 불가
+                if (startConcertDate >= currentDate) {
+                    eventRow.insertCell().appendChild(deleteButton);
+                } else {
+                    eventRow.insertCell().textContent = '취소 불가';
+                }
+
+                break;
+
+            case 'localfestival':
+                // 지역 축제에 대한 데이터를 생성
+
+                const startDate = new Date(event.startDate);
+
+                eventRow.insertCell().textContent = event.name;
+                eventRow.insertCell().textContent = event.org;
+                eventRow.insertCell().textContent = event.location;
+                eventRow.insertCell().textContent = event.startDate;
+                eventRow.insertCell().textContent = event.lastDate;
+
+                // 현재 날짜 이전 예약은 취소 불가
+                if (startDate >= currentDate) {
+                    eventRow.insertCell().appendChild(deleteButton);
+                } else {
+                    eventRow.insertCell().textContent = '취소 불가';
+                }
+
+                break;
+
+            default:
+                break;
+        }
+    });
+
+    createPagination(events.length, eventsPerPage, currentPage, category);
+}
+
+function showColumns(category) {
+    const columns = document.querySelectorAll('th');
+    const categoryColumns = document.querySelectorAll(`th.${category}`);
+
+    // 모든 컬럼을 숨깁니다.
+    for (let i = 0; i < columns.length; i++) {
+        columns[i].style.display = 'none';
     }
 
-    function showColumns(category) {
-        const columns = document.querySelectorAll('th');
-        const categoryColumns = document.querySelectorAll(`th.${category}`);
-
-        // 모든 컬럼을 숨깁니다.
-        for (let i = 0; i < columns.length; i++) {
-            columns[i].style.display = 'none';
-        }
-
-        // 선택한 행사의 컬럼만 표시합니다.
-        for (let i = 0; i < categoryColumns.length; i++) {
-            categoryColumns[i].style.display = 'table-cell';
-        }
+    // 선택한 행사의 컬럼만 표시합니다.
+    for (let i = 0; i < categoryColumns.length; i++) {
+        categoryColumns[i].style.display = 'table-cell';
     }
+}
 
-    function deleteConcert(category,eventId){
-        fetch(`/profile/admin/management?category=${category.toLowerCase()}&id=${eventId}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+function deleteConcert(category, eventId) {
+    fetch(`/profile/admin/management?category=${category.toLowerCase()}&id=${eventId}`, {
+        method: 'DELETE', headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(() => {
+            alert('행사 삭제가 완료되었습니다.');
+            getEvents(category);
         })
-            .then(() => {
-                alert('행사 삭제가 완료되었습니다.');
-                getEvents(category);
-            })
-            .catch((error) => {
-                console.error('행사 삭제 중 에러가 발생했습니다.', error);
-            });
-    }
+        .catch((error) => {
+            console.error('행사 삭제 중 에러가 발생했습니다.', error);
+        });
+}
 
-    function createPagination(totalEvents, eventsPerPage, currentPage, category) {
-        const totalPages = Math.ceil(totalEvents / eventsPerPage);
-        const paginationEl = document.querySelector('.pagination');
+function createPagination(totalEvents, eventsPerPage, currentPage, category) {
+    const totalPages = Math.ceil(totalEvents / eventsPerPage);
+    const paginationEl = document.querySelector('.pagination');
 
-        paginationEl.innerHTML = '';
+    paginationEl.innerHTML = '';
 
-        for (let i = 1; i <= totalPages; i++) {
-            const li = document.createElement('li');
-            const a = document.createElement('a');
-            a.href = '#';
-            a.textContent = i;
-            if (i === currentPage) {
-                li.classList.add('active');
-            }
-            a.addEventListener('click', (event) => {
-                event.preventDefault();
-                getEvents(category, i);
-                scrollToTop();
-            });
-            li.appendChild(a);
-            paginationEl.appendChild(li);
+    for (let i = 1; i <= totalPages; i++) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = '#';
+        a.textContent = i;
+        if (i === currentPage) {
+            li.classList.add('active');
         }
+        a.addEventListener('click', (event) => {
+            event.preventDefault();
+            getEvents(category, i);
+            scrollToTop();
+        });
+        li.appendChild(a);
+        paginationEl.appendChild(li);
     }
+}
 
 function showColumns(category) {
     const columns = document.querySelectorAll('th');
@@ -244,7 +242,7 @@ function updateDropdownOptions(category) {
 const logoutBtn = document.getElementById("logoutBtn");
 logoutBtn.setAttribute("href", "/logout");
 logoutBtn.onclick = function () {
-    fetch('/logout', { method: 'POST', credentials: 'include' })
+    fetch('/logout', {method: 'POST', credentials: 'include'})
         .then(response => {
             if (response.ok) {
                 // 세션 스토리지에서 토큰 제거
