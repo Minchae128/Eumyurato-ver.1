ef0a575d77f28494d266e92f81d51d6fe61e5ff7
smc <minchae.shin1@gmail.com>
2023-07-02 15:36:58 +0900

    profile(마이페이지 - 관리자 - 회원 종합 차트)

--- src/main/resources/static/js/profile/profile_admin_graph.js
+++ src/main/resources/static/js/profile/profile_admin_graph.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function () {
     getCommonMember();
 });
 
-$('#commonBtn').on('click', function() {
+$('#commonBtn').on('click', function () {
     $('#common').show();
     $('#artist').hide();
 
@@ -11,7 +11,7 @@ $('#commonBtn').on('click', function() {
     getCommonMember();
 });
 
-$('#artistBtn').on('click', function() {
+$('#artistBtn').on('click', function () {
     $('#common').hide();
     $('#artist').show();
 
@@ -23,25 +23,20 @@ $('#artistBtn').on('click', function() {
 //일반회원 데이터 불러오기
 function getCommonMember() {
     $.ajax({
-        url: '/profile/admin/total/commonMember',
-        type: 'GET',
-        success: function(response) {
+        url: '/profile/admin/total/commonMember', type: 'GET', success: function (response) {
             drawGenderChart(response, 'commonGenderChart');
             drawRoadChart(response, 'commonRoadChart');
             drawGenreChart(response, 'commonGenreChart');
-        },
-        error: function(error) {
+        }, error: function (error) {
             console.log(error);
         },
     });
 }
 
 //아티스트회원 데이터 불러오기
-function getArtistMember(){
+function getArtistMember() {
     $.ajax({
-        url: '/profile/admin/total/artistMember',
-        type: 'GET',
-        success: function(response) {
+        url: '/profile/admin/total/artistMember', type: 'GET', success: function (response) {
             drawGenderChart(response, 'aritstGenderChart');
             drawGenreChart(response, 'artistGenreChart');
             drawPointChart(response, 'artistPointChart');
@@ -56,8 +51,7 @@ function getArtistMember(){
             const buskAll = response.buskingAll[0].all_busking;
             const buskEl = document.getElementById('buskingCount');
             buskEl.innerHTML = `전체 버스킹 수 : ${buskAll}`;
-        },
-        error: function(error) {
+        }, error: function (error) {
             console.log(error);
         },
     });
@@ -82,21 +76,14 @@ function drawGenderChart(genderData, chartId) {
     }
 
     const genderChart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
+        type: 'doughnut', data: {
             labels: ['여성', '남성'], // 라벨 순서를 변경
-            datasets: [
-                {
-                    label: '성별',
-                    data: genderDataArray,
-                    backgroundColor: ['#FF6384', '#36A2EB'], // 그래프 색깔 순서를 변경
-                    borderWidth: 1,
-                },
-            ],
-        },
-        options: {
-            maintainAspectRatio: false,
-            responsive: false
+            datasets: [{
+                label: '성별', data: genderDataArray, backgroundColor: ['#FF6384', '#36A2EB'], // 그래프 색깔 순서를 변경
+                borderWidth: 1,
+            },],
+        }, options: {
+            maintainAspectRatio: false, responsive: false
         }
     });
 }
@@ -121,21 +108,15 @@ function drawRoadChart(roadData, chartId) {
     }
 
     const sourceChart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: roadLabels,
-            datasets: [
-                {
-                    label: '유입경로',
-                    data: roadDataArray,
-                    backgroundColor: ['#FFCE56', '#4BC0C0', '#FF6384', '#36A2EB'],
-                    borderWidth: 1,
-                },
-            ],
-        },
-        options: {
-            maintainAspectRatio: false,
-            responsive: false
+        type: 'doughnut', data: {
+            labels: roadLabels, datasets: [{
+                label: '유입경로',
+                data: roadDataArray,
+                backgroundColor: ['#FFCE56', '#4BC0C0', '#FF6384', '#36A2EB'],
+                borderWidth: 1,
+            },],
+        }, options: {
+            maintainAspectRatio: false, responsive: false
         }
     });
 }
@@ -157,27 +138,15 @@ function drawGenreChart(genreData, chartId) {
     }
 
     const genreChart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: ['가요', '발라드', '힙합', '댄스', '기타'],
-            datasets: [
-                {
-                    label: '장르',
-                    data: genreDataArray,
-                    backgroundColor: [
-                        '#FF6384',
-                        '#4BC0C0',
-                        '#FFCE56',
-                        '#E7E9ED',
-                        '#36A2EB'
-                    ],
-                    borderWidth: 1,
-                },
-            ],
-        },
-        options: {
-            maintainAspectRatio: false,
-            responsive: false
+        type: 'doughnut', data: {
+            labels: ['가요', '발라드', '힙합', '댄스', '기타'], datasets: [{
+                label: '장르',
+                data: genreDataArray,
+                backgroundColor: ['#FF6384', '#4BC0C0', '#FFCE56', '#E7E9ED', '#36A2EB'],
+                borderWidth: 1,
+            },],
+        }, options: {
+            maintainAspectRatio: false, responsive: false
         }
     });
 }
@@ -187,7 +156,7 @@ function drawPointChart(pointData, chartId) {
     const points = pointData.points;
 
 // points 배열을 point 내림차순으로 정렬
-    points.sort(function(a, b) {
+    points.sort(function (a, b) {
         return b.point - a.point;
     });
 
@@ -198,46 +167,18 @@ function drawPointChart(pointData, chartId) {
     const myChart = new Chart(ctx, {
         type: 'bar', // 막대 그래프 유형 설정
         data: {
-            labels: points.map(item => `${item.nid}(${item.id})`),
-            datasets: [
-                {
-                    label: '포인트',
-                    data: points.map(item => item.point),
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)',
-                        'rgba(255, 0, 255, 0.2)',
-                        'rgba(0, 255, 255, 0.2)',
-                        'rgba(0, 255, 0, 0.2)',
-                        'rgba(128, 0, 0, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)',
-                        'rgba(255, 0, 255, 1)',
-                        'rgba(0, 255, 255, 1)',
-                        'rgba(0, 255, 0, 1)',
-                        'rgba(128, 0, 0, 1)'
-                    ],
-                    borderWidth: 1
-                }
-            ]
-        },
-        options: {
-            responsive: true,
-            scales: {
+            labels: points.map(item => `${item.nid}(${item.id})`), datasets: [{
+                label: '포인트',
+                data: points.map(item => item.point),
+                backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 0, 255, 0.2)', 'rgba(0, 255, 255, 0.2)', 'rgba(0, 255, 0, 0.2)', 'rgba(128, 0, 0, 0.2)'],
+                borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)', 'rgba(255, 0, 255, 1)', 'rgba(0, 255, 255, 1)', 'rgba(0, 255, 0, 1)', 'rgba(128, 0, 0, 1)'],
+                borderWidth: 1
+            }]
+        }, options: {
+            responsive: true, scales: {
                 yAxis: [{
                     ticks: {
-                        beginAtZero: true,
-                        max: Math.ceil(points[0].point / 100) * 100 // 최대 포인트값의 올림 숫자로 y축 설정
+                        beginAtZero: true, max: Math.ceil(points[0].point / 100) * 100 // 최대 포인트값의 올림 숫자로 y축 설정
                     }
                 }]
             }
@@ -249,7 +190,7 @@ function drawPointChart(pointData, chartId) {
 const logoutBtn = document.getElementById("logoutBtn");
 logoutBtn.setAttribute("href", "/logout");
 logoutBtn.onclick = function () {
-    fetch('/logout', { method: 'POST', credentials: 'include' })
+    fetch('/logout', {method: 'POST', credentials: 'include'})
         .then(response => {
             if (response.ok) {
                 // 세션 스토리지에서 토큰 제거
