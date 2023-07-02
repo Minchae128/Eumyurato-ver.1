4a6aadd0d295033fdb936abbaaff4ed014a4250b
smc <minchae.shin1@gmail.com>
2023-07-02 15:14:35 +0900

    user(일반회원가입)

--- src/main/resources/static/js/user/join/Join_common.js
+++ src/main/resources/static/js/user/join/Join_common.js
@@ -308,8 +308,7 @@ joinButton.addEventListener('click', async function (e) {
 
     try {
         const response = await fetch('/loginjoin/common/join', {
-            method: 'POST',
-            body: formData
+            method: 'POST', body: formData
         });
 
         if (response.ok) {
